f016ea08c480bc910d1a133e0b3d99eba7fff832
khaledzamzam <khaled-yahia-zamzam@hotmail.com>
2025-04-10 13:20:02 +0200

    changed closeApp function

--- src/main/handlers.ts
+++ src/main/handlers.ts
@@ -4,37 +4,33 @@ import { uploadFile } from './lib/uploader/upload'
 
 export const registerHandlers = (): void => {
   ipcMain.on('minimizeApp', (event) => {
-    const win = BrowserWindow.fromWebContents(event.sender);
+    const win = BrowserWindow.fromWebContents(event.sender)
     win?.minimize()
   })
 
   ipcMain.on('maximizeApp', (event) => {
-    const win = BrowserWindow.fromWebContents(event.sender);
-    if(win?.isMaximized()) {
-      win?.unmaximize();
+    const win = BrowserWindow.fromWebContents(event.sender)
+    if (win?.isMaximized()) {
+      win?.unmaximize()
     } else {
       win?.maximize()
     }
   })
 
   ipcMain.on('closeApp', (event) => {
-    const win = BrowserWindow.fromWebContents(event.sender);
-    if (win && !win.isDestroyed()) {
-      win.close();
-    } else {
-      console.log('Window already closed or destroyed.');
-    }
+    const win = BrowserWindow.fromWebContents(event.sender)
+    win?.close()
   })
 
   ipcMain.handle('selectDirectory', selectDirectory)
 
-  ipcMain.handle('readFiles', async (_, dirPath: string) => readFiles(dirPath));
+  ipcMain.handle('readFiles', async (_, dirPath: string) => readFiles(dirPath))
 
   ipcMain.handle('uploadFiles', async (event, filePath, metadata, accessToken) => {
     try {
-      await uploadFile(event, filePath, metadata, accessToken);
-    } catch(e: any) {
-      throw Error(e);
+      await uploadFile(event, filePath, metadata, accessToken)
+    } catch (e: any) {
+      throw Error(e)
     }
   })
 }
