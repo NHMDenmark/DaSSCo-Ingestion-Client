6752b282a6219b817732a7799a3f4b10d72b0f9a
khaledzamzam <khaled-yahia-zamzam@hotmail.com>
2025-07-02 15:40:10 +0200

    change login page

--- src/renderer/src/pages/login/Login.tsx
+++ src/renderer/src/pages/login/Login.tsx
@@ -20,20 +20,23 @@ const Login = () => {
   }, [])
 
   const html =
-  '<p>Login via your keycloak credentials.&nbsp;<br>To get a login, contact support@dassco.dk with&nbsp;<br>Subject: New login for Ingestion Client&nbsp;<br>Please create a new Login with Username: XXXXXX&nbsp;<br>Full Name of the Ingestor:&nbsp;<br>Email:&nbsp;<br>Reason for the new login</p><p>Email will be replied to with a password.&nbsp;<br>Change the password on first login into Keycloak pop-up window.</p>'
+    '<p>Login via your keycloak credentials.&nbsp;<br>To get a login, contact support@dassco.dk with&nbsp;<br>Subject: New login for Ingestion Client&nbsp;<br>Please create a new Login with Username: XXXXXX&nbsp;<br>Full Name of the Ingestor:&nbsp;<br>Email:&nbsp;<br>Reason for the new login</p><p>Email will be replied to with a password.&nbsp;<br>Change the password on first login into Keycloak pop-up window.</p>'
 
   return (
-    <Stack p="lg">
-      <Title order={3}>Welcome to Ingestion Client Version {appVersion} </Title>
+    <Center>
+      <Stack p="lg">
 
-      <TypographyStylesProvider>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </TypographyStylesProvider>
+        <Title order={3}>Welcome to Ingestion Client Version {appVersion} </Title>
 
-      <Center mt={19}>
-        <Button onClick={login}>Sign in</Button>
-      </Center>
-    </Stack>
+        <TypographyStylesProvider>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </TypographyStylesProvider>
+
+        <Center mt={19}>
+          <Button onClick={login}>Sign in</Button>
+        </Center>
+      </Stack>
+    </Center>
   )
 }
 
